e51f3cc89f9dcfebf6900ca26c2489a81fcb2591
arash afaghi <blueman@gmail.com>
2012-09-20 13:50:18 +0200

    added clock

---
+++ lab2/Intranet/Scripts/clock.js
@@ -0,0 +1,26 @@
+﻿function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    // add a zero in front of numbers<10
+    m = checkTime(m);
+    s = checkTime(s);
+
+    var day = today.getDate();
+   
+    var mon = today.getMonth();
+    var year = today.getYear();
+    year = (year < 1000) ? year + 1900 : year;
+    document.getElementById('clock').innerHTML = year + '/' + mon + '/' + day + '--' + h + ":" + m + ":" + s;
+    document.getElementById('bigclock').innerHTML = year + '/' + mon + '/' + day + '--' + h + ":" + m + ":" + s;
+    t = setTimeout(function () {
+        startTime()
+    }, 500);
+}
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
