92a86a4554f10f423f9a6e68fdcb66a782923951
arash afaghi <blueman@gmail.com>
2012-09-26 11:31:45 +0200

    added  gamecontroller js/css

---
+++ lab2/Intranet/Controllers/GameController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Intranet.Controllers
+{
+    public class GameController : Controller
+    {
+        //
+        // GET: /Game/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Play()
+        {
+            return null;
+        }
+
+    }
+}
